633a0441238ec64bbfc18a28bdf0e96643d617e0
Kezikov Boris <boris200898@icloud.com>
2019-12-11 01:46:12 +0300

    Pronounce answer

--- app/src/main/java/com/example/androidgameapp/MainActivity.java
+++ app/src/main/java/com/example/androidgameapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -14,6 +15,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
@@ -29,10 +31,19 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Locale;
+import java.util.UUID;
 
 public class MainActivity extends Activity {
+
+    private TextToSpeech textToSpeech;
+
     protected static final int RESULT_SPEECH = 1;
-    private Cursor c = null;
+
+    private static String myURL = "https://kezikov-spring-test.herokuapp.com/android?letter=";
+
+    private static String lastLetter = null;
+
 
     private Button btnSpeak;
     private TextView txtText;
@@ -42,8 +53,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Toolbar toolbar = findViewById(R.id.toolbar);
-        // setSupportActionBar(toolbar);
         txtText = findViewById(R.id.txtText);
         btnSpeak = findViewById(R.id.mainButton);
         btnSpeak.setOnClickListener(new View.OnClickListener() {
@@ -52,36 +61,34 @@ public class MainActivity extends Activity {
                 Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
                 intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
                 startActivityForResult(intent, RESULT_SPEECH);
-                txtText.setText("");
             }
         });
 
+    }
 
-        new AsyncTask<Void, String, String>() {
-            @Override
-            protected String doInBackground(Void... voids) {
-                System.out.println("hjkl");
-                String s = "";
-                try {
-                    String myURL = "https://kezikov-spring-test.herokuapp.com/android";
-                    s = doGet(myURL);
-                } catch (IOException e) {
-                    Toast.makeText(getApplicationContext(),"Cannot load cities list",Toast.LENGTH_LONG).show();
-                }
-                return s;
-            }
+    @Override
+    public void onPause() {
+        if (textToSpeech != null) {
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+        super.onPause();
+    }
 
-            @Override
-            protected void onPostExecute(final String result) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        txtText.setText("");
-                    }
-                });
-            }
-        }.execute();
+    @Override
+    public void onStop() {
+        if (textToSpeech != null) {
+            textToSpeech.stop();
+        }
+        super.onStop();
+    }
 
+    @Override
+    public void onDestroy() {
+        if (textToSpeech != null) {
+            textToSpeech.shutdown();
+        }
+        super.onDestroy();
     }
 
     @Override
@@ -95,7 +102,11 @@ public class MainActivity extends Activity {
                             .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
 
                     assert text != null;
-                    txtText.setText(text.get(0));
+                    String userCity = text.get(0);
+                    lastLetter = userCity.substring(userCity.length() - 1).toUpperCase();
+                    callServer();
+
+                    //txtText.setText(newCity);
                 }
                 break;
             }
@@ -121,12 +132,55 @@ public class MainActivity extends Activity {
         }
         bufferedReader.close();
 
-//      print result
         System.out.println("Response string: " + response.toString());
 
 
         return response.toString();
     }
+
+    @SuppressLint("StaticFieldLeak")
+    private void callServer() {
+        new AsyncTask<Void, String, String>() {
+            @Override
+            protected String doInBackground(Void[] voids) {
+                String s = "";
+                try {
+                    s = doGet(myURL + lastLetter);
+                } catch (IOException e) {
+                    Toast.makeText(getApplicationContext(), "Cannot load cities list", Toast.LENGTH_LONG).show();
+                }
+                return s;
+            }
+
+            @Override
+            protected void onPostExecute(final String result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        txtText.setText(result);
+                        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                            @Override
+                            public void onInit(int status) {
+                                Log.e("TTS", "TextToSpeech.OnInitListener.onInit...");
+                                speakOut(result);
+
+
+                            }
+                        });
+                    }
+                });
+            }
+        }.execute();
+    }
+
+    private void speakOut(String text) {
+        // Text to Speak
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+        // A random String (Unique ID).
+        String utteranceId = UUID.randomUUID().toString();
+        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, utteranceId);
+    }
+
 
 }
 
